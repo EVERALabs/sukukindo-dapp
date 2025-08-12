a4a655ae6e4c6b6b43144d6ee751b0cfca3ae37b
Arjuna Marcelino <arjunamarcelino@gmail.com>
2025-08-12 22:22:07 +0700

    fix: file location

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import HomeStat from "../_components/home/HomeStat"
+import HomeYield from "../_components/home/HomeYield"
+
 export default function HomePage() {
     return (
-        <div className="min-h-full text-white pr-6">
-            <div className="grid lg:grid-cols-5 gap-4">
+        <div className="min-h-screen bg-gray-950 text-white p-4 sm:p-6">
+            <div className="max-w-5xl mx-auto mt-24 space-y-6 sm:space-y-8">
+                <HomeStat />
+                <HomeYield />
             </div>
         </div>
     )
